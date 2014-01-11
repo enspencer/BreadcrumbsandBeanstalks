c933750e514ca35063d92a7cb9e76e1cf4a3d59e
Emma Spencer <emma.n.spencer@gmail.com>
2014-01-11 16:56:23 -0500

    created books controller and example view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Breadcrumbsbeanstalks::Application.routes.draw do
+  get "books/example"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
