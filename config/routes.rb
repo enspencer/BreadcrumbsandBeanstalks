3ac26621028e6ffdb69ef687ffbcc2badd9c2268
Emma Spencer <emma.n.spencer@gmail.com>
2014-01-11 16:59:21 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Breadcrumbsbeanstalks::Application.routes.draw do
 
   resources :books
-  get "books/example"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
